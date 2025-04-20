773d27f0d660dc76adbbe8674c46d95040d60f62
Alan <alanbrum2807@gmail.com>
2025-04-20 12:08:11 -0300

    Criação do projeto

---
+++ src/main/java/br/com/brum/api_rest_calc/ApiRestCalcApplication.java
@@ -0,0 +1,13 @@
+package br.com.brum.api_rest_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brum/api_rest_calc/ApiRestCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brum.api_rest_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
